a450ae89e93b7dc3f3cc772f51bc6d95bc056078
Reena T. Desai <44991364+r33nava@users.noreply.github.com>
2019-03-05 19:53:32 -0500

    Add files via upload

--- ETLProjectScript.sql
+++ ETLProjectScript.sql
@@ -9,4 +9,6 @@ select * from dcwards_cleaned;
 select * from dccrimes2019_cleaned;
 
 
-
+SELECT dccrimes2019_cleaned.OFFENSE, dccrimes2019_cleaned.SHIFT, dcwards_cleaned.WARD, dcwards_cleaned.AvgFamilyIncome
+FROM dccrimes2019_cleaned
+INNER JOIN dcwards_cleaned ON dccrimes2019_cleaned.WARD=dcwards_cleaned.WARD;
